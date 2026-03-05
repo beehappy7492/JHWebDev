27d5a8b632cbebc520074d4ae4fd5d4c1ec209a6
beehappy7492 <beehappy7492@gmail.com>
2026-03-05 19:41:27 +0000

    change vite config for hosting

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/JHWebDev/',
-  build: {
-    outDir: '../docs',
-    emptyOutDir: true,
-  },
 })
